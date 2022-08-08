0dc1fd27a3878cb2491d83c045dcc83651b02c88
Eric Murano <2396940+emurano@users.noreply.github.com>
2022-08-09 00:51:25 +1000

    Separate controller and routes to their own files

--- index.ts
+++ index.ts
@@ -1,33 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import { query, validationResult } from 'express-validator';
-import isValidLongitude from './src/validators/longitude-validator';
+import { locationsRouter } from './src/controllers/locations/locations.router';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello from express and typescript');
-});
-
-app.get(
-  '/locations',
-  query('swlat')
-    .exists()
-    .withMessage('south-west latitude is required')
-    .isNumeric()
-    .withMessage('south-west latitude must be a number')
-    .custom(isValidLongitude)
-  ,
-  query('swlng').exists().isNumeric(),
-  (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array({ onlyFirstError: true }) });
-    }
-    res.send('Did it work?');
-  });
+app.use('/locations', locationsRouter);
 
 const port = process.env.PORT || 3000;
 
---
+++ src/controllers/locations/locations.router.ts
@@ -0,0 +1,43 @@
+import express, { NextFunction, Request, Response } from 'express';
+import { query, validationResult } from 'express-validator';
+import isValidLongitude from '../../validators/longitude-validator';
+import { getLocations } from './locations.controller';
+import { handleValidationErrors } from '../validation-error-handler';
+
+const validationRules = [
+    query('swlat')
+      .exists()
+      .withMessage('south-west latitude is required')
+      .isNumeric()
+      .withMessage('south-west latitude must be a number')
+      .custom(isValidLongitude)
+    ,
+    query('swlng')
+      .exists()
+      .withMessage('south-west longitude is required')
+      .isNumeric()
+      .withMessage('south-west longitude must be a number')
+      .custom(isValidLongitude),
+
+    query('nelat')
+      .exists()
+      .withMessage('north-east latitude is required')
+      .isNumeric()
+      .withMessage('north-east latitude must be a number')
+      .custom(isValidLongitude)
+    ,
+    query('nelng')
+      .exists()
+      .withMessage('north-east longitude is required')
+      .isNumeric()
+      .withMessage('north-east longitude must be a number')
+      .custom(isValidLongitude),
+
+    handleValidationErrors
+];
+
+const locationsRouter = express.Router();
+
+locationsRouter.route('').get(validationRules, getLocations);
+
+export { locationsRouter };
